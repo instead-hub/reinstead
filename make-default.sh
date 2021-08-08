a6cb6aceb5aa971f5d0b9ae8646755f493c28d57
peter <p.kosyh@gmail.com>
2021-08-08 12:15:05 +0300

    removed __linux__ -D

--- make-default.sh
+++ make-default.sh
@@ -1,4 +1,4 @@
-CFLAGS="`pkg-config --cflags sdl2` -Isrc/lua -Isrc/instead -D__linux__ -Dunix"
+CFLAGS="`pkg-config --cflags sdl2` -Isrc/lua -Isrc/instead -Dunix"
 LDFLAGS="`pkg-config --libs sdl2`"
 gcc -Wall -c -O3 src/*.c src/instead/*.c src/lua/*.c $CFLAGS && \
 gcc *.o $LDFLAGS -lm -o instead9
--- make-luajit.sh
+++ make-luajit.sh
@@ -1,4 +1,4 @@
-CFLAGS="`pkg-config --cflags sdl2` `pkg-config --cflags luajit` -Isrc/instead -D__linux__ -Dunix"
+CFLAGS="`pkg-config --cflags sdl2` `pkg-config --cflags luajit` -Isrc/instead -Dunix"
 LDFLAGS="`pkg-config --libs sdl2` `pkg-config --libs luajit`"
 gcc -Wall -c -O3 src/*.c src/instead/*.c $CFLAGS && \
 gcc *.o $LDFLAGS -lm -o instead9
