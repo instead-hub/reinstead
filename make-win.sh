ddad92611e7b888075cd1a20b4459247a0305601
peter <p.kosyh@gmail.com>
2021-08-12 21:38:09 +0300

    resinstead.ico

--- make-win.sh
+++ make-win.sh
@@ -1,4 +1,8 @@
 CFLAGS="-Isrc/instead -Iwindows/"
 LDFLAGS="windows/libluajit.a -Lwindows/SDL2 -lSDL2 -lSDL2main -lm"
-i686-w64-mingw32-gcc -Wall -O3 src/*.c src/instead/*.c $CFLAGS $LDFLAGS -mwindows -o reinstead.exe
+
+i686-w64-mingw32-windres -i windows/resources.rc -o resources.o || exit 1
+i686-w64-mingw32-gcc -Wall -O3 src/*.c src/instead/*.c resources.o $CFLAGS $LDFLAGS -mwindows -o reinstead.exe || exit 1
+i686-w64-mingw32-strip reinstead.exe
+
 rm -f *.o
