986848b6e26a4694886b4f4f5e69a790b168e335
Peter Kosyh <p.kosyh@gmail.com>
2021-09-16 13:29:57 +0300

    sdk version up

--- contrib/android/app/src/main/java/ru/hugeping/reinstead/Assets.java
+++ contrib/android/app/src/main/java/ru/hugeping/reinstead/Assets.java
@@ -19,18 +19,24 @@ public class Assets
 							   String arg_assetDir,
 							   String arg_destinationDir) throws IOException
 	{
+		BufferedReader reader = null;
 		String dest_dir_path = arg_destinationDir;
 		Log.e("reinstead", "External: " + dest_dir_path);
 		File dest_dir = new File(dest_dir_path);
 		boolean doCopy = true;
-		try (BufferedReader reader = new BufferedReader(new FileReader(dest_dir_path + "/stamp"))) {
+		try {
+			reader = new BufferedReader(new FileReader(dest_dir_path + "/stamp"));
 			String line1 = reader.readLine();
 			Log.e("reinstead", "Internal stamp: " + line1);
 			BufferedReader reader2 = new BufferedReader(new InputStreamReader(asset_manager.open(arg_assetDir + "/stamp")));
 			String line2 = reader2.readLine();
 			Log.e("reinstead", "Assets stamp: " + line2);
 			doCopy = !line2.equals(line1);
-		} catch (IOException io) {}
+		} finally {
+			/* nothing to do */
+		}
+		if (reader == null)
+			doCopy = false;
 
 		if (!doCopy)
 			return dest_dir_path;
