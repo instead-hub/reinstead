6b2e6cb5317debe8573c67a2b7adc4b8be03d460
peter <p.kosyh@gmail.com>
2021-11-17 10:11:33 +0300

    simpler make-emscripten.sh

--- make-emscripten.sh
+++ make-emscripten.sh
@@ -1,5 +1,5 @@
 export WORKSPACE="/home/peter/Devel/emsdk/env"
 . /home/peter/Devel/emsdk/emsdk_env.sh
-emcc -O2 -o reinstead.html src/*.c src/lua/*.c src/instead/*.c -Isrc/lua -Isrc/instead -Dunix -lSDL2 -s USE_SDL=2 -DDATADIR=\"/data\" \
+emcc -O2 -o reinstead.html src/*.c src/lua/*.c src/instead/*.c -Isrc/lua -Isrc/instead -Dunix -s USE_SDL=2 -DDATADIR=\"/data\" \
 -lidbfs.js -s WASM=1 -s SAFE_HEAP=0  -s ALLOW_MEMORY_GROWTH=1 \
 --preload-file data/
